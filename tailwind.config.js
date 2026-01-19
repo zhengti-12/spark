071d2be917b71b82327667c942ea8b9e0f497026
tiffany zheng <tiffanyyouwu@gmail.com>
2026-01-19 12:05:24 -0500

    fixed code 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./public/index.html"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
 
