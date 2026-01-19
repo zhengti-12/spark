63e39292d117c42661e8dbd260f0dfd9d9ed59be
tiffany zheng <tiffanyyouwu@gmail.com>
2026-01-19 01:30:09 -0500

    first functional draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [books, setbooks] = useState([])
+
+  const [title, settitle] = useState('')
+  const [genre, setgenre] = useState('')
+  const [date, setdate] = useState('')
+
+  const [popup, setpopup] = useState(false)
+  const [recgenre, setrec] = useState('')
+  const [wantsrec, setwrec] = useState(false)
+
+  const Addbook = () => {
+    if (title == "") {
+      alert("type a book title")
+    } 
+    else {
+      setpopup(true)
+
+    const newbook = {
+      title: title,
+      genre: genre,
+      date: date
+    }
+
+    setbooks([...books, newbook])
+    settitle('')
+    setgenre('')
+    setdate('')
+   }
+  }
+
+  const closepopup = () => {
+    setpopup(false)
+    setwrec(false)
+    setrec('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "appheader">
+      <div className="book">
+       <h2>Log a new book</h2>
+
+        <input 
+          type ="text" 
+          placeholder ="book title"
+          value = {title}
+          onChange = {(e) => settitle(e.target.value)}
+        />
+
+      <select value={genre} onChange = {(e) => setgenre(e.target.value)}>
+        <option value = "">select a genre</option>
+        <option value = "romance">romance</option>
+        <option value = "comedy">comedy</option>
+        <option value = "sci_fi">sci-fi</option>
+        <option value = "action">action</option>
+        <option value = "horror">horror</option>
+        <option value = "non_fiction">non-fiction</option>
+      </select>
+
+      <input 
+        type ="date"
+        value = {date}
+        onChange = {(e) => setdate(e.target.value)}/>
+
+      <button onClick={Addbook}> add book </button>
+      </div>
+
+      <div className="booklog">
+        <h2>Book Log</h2>
+        <ul className="booklist">
+          {/* This displays every book in your list */}
+          {books.map((book, index) => (
+            <li key={index}>
+              <strong>{book.title}</strong> — {book.genre} (Read on: {book.date})
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {popup && (
+        <div className="popupoverlay">
+          <div className="popupcontent">
+
+            {!wantsrec ? (
+            <>
+            <h3>Book logged! Would you like a recommendation?</h3>
+            <button onClick ={() => setwrec(true)}>yes</button>
+            <button onClick={closepopup}>No</button>
+            </>
+            ) : (
+            <>
+            <p>Pick a genre:</p>
+            <select onChange={(e) => setrec(e.target.value)}>
+              <option value="">Choose</option>
+              <option value="romance">romance</option>
+              <option value="sci_fi">sci-Fi</option>
+              <option value="non_fiction">non-fiction</option>
+              <option value="horror">horror</option>
+              <option value="comedy">comedy</option>
+              <option value="action">action</option>
+            </select>
+
+            {recgenre && (
+              <div className="result">
+                <p>You should read: <b>{recs[recgenre]}</b></p>
+               <button onClick={closepopup}>Exit</button>
+            </div>
+            )}
+          </>
+          )}
+        </div>
+      </div>
+      )}
     </div>
-  );
+  )
+}
+
+const recs ={
+  romance: "Twilight",
+  comedy: "The Princess Bride",
+  action: "The Maze Runner",
+  horror: "It",
+  sci_fi: "The Expanse",
+  non_fiction: "outliers"
 }
 
 export default App;
